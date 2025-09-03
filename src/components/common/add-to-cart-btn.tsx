2e822c45a6ee87521fb69ff6ee0388fa20bf40e2
Mahmouud Khaled <mahmouudkhaled2@gmail.com>
2025-09-03 16:00:59 +0300

    implement add to cart functionality with error handling and user feedback

---
+++ src/components/common/add-to-cart-btn.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { Button } from "../ui/button";
+import { RiShoppingBasketFill } from "react-icons/ri";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { addToCart } from "@/lib/actions/cart.action";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+
+type AddToCartButtonProps = {
+  className?: string;
+  productId: number;
+};
+
+export default function AddToCartButton({ productId, className }: AddToCartButtonProps) {
+  // Translations
+  const t = useTranslations();
+
+  // Mutations
+  const addProductToCart = useMutation({
+    mutationFn: (productId: number) => addToCart(productId),
+    onSuccess: () => {
+      toast.success(t("product-added"));
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <Button
+      onClick={(e) => {
+        e.stopPropagation();
+        // Handle add to cart logic here
+        addProductToCart.mutate(productId);
+      }}
+      className={cn("bg-light-blue flex items-center gap-2 hover:bg-blue-700", className)}
+    >
+      {addProductToCart.isPending ? (
+        <Loader2 size={18} className="animate-spin" />
+      ) : (
+        <>
+          <RiShoppingBasketFill className="text-main-yellow text-xl" />
+          {t("add-to-cart")}
+        </>
+      )}
+    </Button>
+  );
+}
