b547688b506ce517fb7a4c6b98bb20107d88a2b0
Ziadd-zamel <ziaddmahmoude@gmail.com>
2025-09-09 12:16:47 +0200

    add forget password flow

--- src/components/auth/forget-password-form.tsx
+++ src/components/auth/forget-password-form.tsx
@@ -2,14 +2,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useTranslations } from "next-intl";
 import { useMutation } from "@tanstack/react-query";
@@ -18,7 +11,13 @@ import { toast } from "sonner";
 import { ForgotPasswordFields, useForgotPasswordSchema } from "@/lib/schemas/auth.schema";
 import { forgotPasswordUser } from "@/lib/actions/auth.actions";
 
-export default function ForgotPasswordForm() {
+export default function ForgotPasswordForm({
+  onNext,
+  setPhoneNumber,
+}: {
+  onNext: () => void;
+  setPhoneNumber: (phone: string) => void;
+}) {
   // Schema
   const forgotPasswordSchema = useForgotPasswordSchema();
 
@@ -32,6 +31,7 @@ export default function ForgotPasswordForm() {
       toast.success(t("otp-sent"), {
         description: t("check-phone"),
       });
+      onNext();
     },
     onError: () => {
       toast.error(t("otp-failed"));
@@ -48,42 +48,43 @@ export default function ForgotPasswordForm() {
 
   const onSubmit: SubmitHandler<ForgotPasswordFields> = async (values) => {
     mutation.mutate(values);
+    setPhoneNumber(values.phone);
   };
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="mx-auto flex w-full max-w-lg flex-col items-center justify-center sm:px-5"
+        className="mx-auto flex w-full max-w-lg flex-col items-center justify-center"
       >
-        <h2 className="mb-6 flex w-full justify-center border-b-2 py-3 text-center text-lg font-medium">
+        <h2 className="mb-6 flex w-full justify-center border-b-2 py-3 text-center text-2xl font-medium">
           {t("forgot-password-title")}
         </h2>
-
-        <div className="w-full space-y-6">
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel className="text-lg">{t("phone")}</FormLabel>
-                <FormControl>
-                  <Input
-                    className="border-[#F0EEF0]"
-                    variant="outline"
-                    placeholder={t("phone-placeholder")}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="w-full px-3 pb-4 sm:px-8">
+          <p className="mt-5 mb-3 w-full items-start font-medium">{t("retrieve-password")}</p>
+          <div className="w-full space-y-6">
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      className="border-[#F0EEF0]"
+                      variant="outline"
+                      placeholder={t("phone-placeholder")}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button className="mt-6 w-full py-6" type="submit" disabled={mutation.isPending}>
+            {mutation.isPending ? t("sending") : t("send-otp")}
+          </Button>
         </div>
-
-        <Button className="mt-10 w-full py-4" type="submit" disabled={mutation.isPending}>
-          {mutation.isPending ? t("sending") : t("send-otp")}
-        </Button>
       </form>
     </Form>
   );
