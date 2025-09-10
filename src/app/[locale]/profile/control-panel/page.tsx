e41d34112f822ce5b557050868fb10e0feb3b79c
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-09-10 13:50:04 +0300

    fix issues remvove package-lock

--- src/app/[locale]/profile/control-panel/page.tsx
+++ src/app/[locale]/profile/control-panel/page.tsx
@@ -6,8 +6,6 @@ import { getUserDetails } from "@/lib/actions/profile.actions";
 export default async function Page() {
   const userData = await getUserDetails();
 
-  console.log("userdata", userData);
-
   if (!userData) return <p className="font-xl text-center text-red-500"> User Not Found</p>;
 
   return (
