39b313d605855c1e14755a3ce2025a0519cfc375
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-09-16 13:42:33 +0300

    implement infinte quires

--- src/components/layout/header/_components/notifications-dropdown.tsx
+++ src/components/layout/header/_components/notifications-dropdown.tsx
@@ -8,10 +8,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Link } from "@/i18n/routing";
 import { getUserNotifications, markNotificationAsRead } from "@/lib/actions/notification.actions";
 import { cn } from "@/lib/utils";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { intlFormatDistance } from "date-fns";
 import { CheckCheck, Loader2 } from "lucide-react";
 import { useLocale } from "next-intl";
@@ -19,59 +20,29 @@ import React, { useState } from "react";
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdOutlineNotificationsActive } from "react-icons/md";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { toast } from "sonner";
 
-type RepairRequestData = {
-  message: string;
-  code: string;
-  date: string; // "2025-09-14 15:30:41"
-};
-
-type CustomNotificationData = {
-  title: string;
-  body: string;
-  product_id: number;
-  image: string;
-};
-
-type BaseNotification<T extends string, D> = {
-  id: string;
-  type: T;
-  data: D;
-  read_at: string | null;
-  created_at: string;
-};
-
-export type Notification =
-  | BaseNotification<"RepairRequestNotification", RepairRequestData>
-  | BaseNotification<"CustomNotification", CustomNotificationData>;
-
-export type NotificationResponse = {
-  success: boolean;
-  message: string;
-  data: {
-    data: Notification[];
-    pagination: {
-      page: number;
-      total_pages: number;
-      total_items: number;
-      limit: number;
-    };
-  };
-};
-
 export default function NotificationsDropdown() {
-  const locale = useLocale();
-  const queryClient = useQueryClient();
+  // States
   const [selectedId, setSelectedId] = useState("");
+  const queryClient = useQueryClient();
 
-  const { data, isLoading } = useQuery<NotificationResponse>({
+  // Translations
+  const locale = useLocale();
+
+  // Inifnite query all notifications
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
     queryKey: ["notifications"],
     queryFn: getUserNotifications,
-    refetchInterval: 60 * 1000,
-  });
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      const isLastPage = lastPage.data.pagination.page >= lastPage.data.pagination.total_pages;
 
-  const allNotifications = data?.data.data;
+      return isLastPage ? undefined : lastPage.data.pagination.page + 1;
+    },
+    refetchInterval: 6 * 10 * 1000,
+  });
 
   const { mutate, isPending } = useMutation({
     mutationFn: markNotificationAsRead,
@@ -87,6 +58,9 @@ export default function NotificationsDropdown() {
     },
   });
 
+  // Variables
+  const allNotifications = data?.pages.flatMap((page) => page.data.data) ?? [];
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -94,112 +68,137 @@ export default function NotificationsDropdown() {
           <IoMdNotificationsOutline className="text-primary size-7" />
 
           <span className="absolute -end-1.5 -top-1 flex size-5 items-center justify-center rounded-full bg-[#EB5757] text-xs text-white">
-            {allNotifications?.length || 0}
+            {data?.pages[0].data.pagination.total_items || 0}
           </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="mx-5 w-sm rounded-sm p-1">
         <DropdownMenuLabel>Your Notifications</DropdownMenuLabel>
         <DropdownMenuSeparator className="" />
-        <div className="max-h-[400px] space-y-1 overflow-y-auto">
-          {allNotifications?.map((notification) => (
-            <React.Fragment key={notification.id}>
-              {notification.type === "RepairRequestNotification" && (
-                <div
-                  className={cn(
-                    "relative flex min-h-22 flex-col rounded-sm bg-zinc-100 p-2 text-sm",
-                    {
-                      "bg-main/10 border-s-main border-s-4": !notification.read_at,
-                    },
-                  )}
-                >
-                  <p className="text-main mb-1 flex items-center gap-2 font-medium">
-                    <HiOutlineWrenchScrewdriver className="size-4" />
-                    Repair Requrest
-                  </p>
-                  <p className="font-medium">
-                    Code:{" "}
-                    <Link
-                      href={`/profile/previous-orders/?orderType=repair`}
-                      className="text-blue-600 underline-offset-2 hover:underline"
-                    >
-                      {notification.data.code}
-                    </Link>
-                  </p>
-                  <p className="mb-1 leading-4">{notification.data.message}</p>
-                  <p
-                    className={cn("self-end text-xs font-medium text-zinc-600", {
-                      "text-main": !notification.read_at,
-                    })}
-                  >
-                    {intlFormatDistance(new Date(notification.created_at), new Date(), {
-                      locale: locale,
-                    })}
-                  </p>
+        <div id="scrollableDiv" className="max-h-[400px] space-y-1 overflow-y-auto">
+          {isLoading &&
+            Array.from({ length: 3 }).map((_, i) => (
+              <Skeleton key={i} className="h-22 w-full"></Skeleton>
+            ))}
 
-                  {!notification.read_at && (
-                    <Button
-                      onClick={() => {
-                        setSelectedId(notification.id);
-                        mutate(notification.id);
-                      }}
-                      disabled={isPending && selectedId === notification.id}
-                      className="absolute end-1 top-1 size-6 rounded-sm"
-                    >
-                      {isPending && selectedId === notification.id ? (
-                        <Loader2 className="size-4 animate-spin" />
-                      ) : (
-                        <CheckCheck className="size-4" />
-                      )}
-                    </Button>
-                  )}
+          {allNotifications.length !== 0 ? (
+            <InfiniteScroll
+              dataLength={allNotifications.length}
+              next={fetchNextPage}
+              hasMore={!!hasNextPage}
+              loader={
+                <div className="space-y-1 pt-1">
+                  {Array.from({ length: 3 }).map((_, i) => (
+                    <Skeleton key={i} className="h-22 w-full rounded-md"></Skeleton>
+                  ))}
                 </div>
-              )}
+              }
+              scrollableTarget="scrollableDiv"
+            >
+              <div className="space-y-1">
+                {allNotifications?.map((notification) => (
+                  <React.Fragment key={notification.id}>
+                    {notification.type === "RepairRequestNotification" && (
+                      <div
+                        className={cn(
+                          "relative flex min-h-22 flex-col rounded-sm bg-zinc-100 p-2 text-sm",
+                          {
+                            "bg-main/10 border-s-main border-s-4": !notification.read_at,
+                          },
+                        )}
+                      >
+                        <p className="text-main mb-1 flex items-center gap-2 font-medium">
+                          <HiOutlineWrenchScrewdriver className="size-4" />
+                          Repair Requrest
+                        </p>
+                        <p className="font-medium">
+                          Code:{" "}
+                          <Link
+                            href={`/profile/previous-orders/?orderType=repair`}
+                            className="text-blue-600 underline-offset-2 hover:underline"
+                          >
+                            {notification.data.code}
+                          </Link>
+                        </p>
+                        <p className="mb-1 leading-4">{notification.data.message}</p>
+                        <p
+                          className={cn("self-end text-xs font-medium text-zinc-600", {
+                            "text-main": !notification.read_at,
+                          })}
+                        >
+                          {intlFormatDistance(new Date(notification.created_at), new Date(), {
+                            locale: locale,
+                          })}
+                        </p>
 
-              {notification.type === "CustomNotification" && (
-                <div
-                  className={cn(
-                    "relative flex min-h-22 flex-col rounded-sm bg-zinc-100 p-2 text-sm",
-                    {
-                      "bg-main/10 border-s-main border-s-4": !notification.read_at,
-                    },
-                  )}
-                >
-                  <p className="text-main mb-1 flex items-center gap-2 font-medium">
-                    <MdOutlineNotificationsActive className="size-5" />
-                    Custome Notification
-                  </p>
-                  <p className="font-medium">{notification.data.title}</p>
-                  <p className="mb-1 leading-4">{notification.data.body}</p>
-                  <p
-                    className={cn("self-end text-xs font-medium text-zinc-600", {
-                      "text-main": !notification.read_at,
-                    })}
-                  >
-                    {intlFormatDistance(new Date(notification.created_at), new Date(), {
-                      locale: locale,
-                    })}
-                  </p>
-                  {!notification.read_at && (
-                    <Button
-                      onClick={() => {
-                        setSelectedId(notification.id);
-                        mutate(notification.id);
-                      }}
-                      disabled={isPending && selectedId === notification.id}
-                      className="absolute end-1 top-1 size-6 rounded-sm"
-                    >
-                      {isPending && selectedId === notification.id ? (
-                        <Loader2 className="size-4 animate-spin" />
-                      ) : (
-                        <CheckCheck className="size-4" />
-                      )}
-                    </Button>
-                  )}
-                </div>
-              )}
-            </React.Fragment>
-          ))}
+                        {!notification.read_at && (
+                          <Button
+                            onClick={() => {
+                              setSelectedId(notification.id);
+                              mutate(notification.id);
+                            }}
+                            disabled={isPending && selectedId === notification.id}
+                            className="absolute end-1 top-1 size-6 rounded-sm"
+                          >
+                            {isPending && selectedId === notification.id ? (
+                              <Loader2 className="size-4 animate-spin" />
+                            ) : (
+                              <CheckCheck className="size-4" />
+                            )}
+                          </Button>
+                        )}
+                      </div>
+                    )}
+
+                    {notification.type === "CustomNotification" && (
+                      <div
+                        className={cn(
+                          "relative flex min-h-22 flex-col rounded-sm bg-zinc-100 p-2 text-sm",
+                          {
+                            "bg-main/10 border-s-main border-s-4": !notification.read_at,
+                          },
+                        )}
+                      >
+                        <p className="text-main mb-1 flex items-center gap-2 font-medium">
+                          <MdOutlineNotificationsActive className="size-5" />
+                          Custome Notification
+                        </p>
+                        <p className="font-medium">{notification.data.title}</p>
+                        <p className="mb-1 leading-4">{notification.data.body}</p>
+                        <p
+                          className={cn("self-end text-xs font-medium text-zinc-600", {
+                            "text-main": !notification.read_at,
+                          })}
+                        >
+                          {intlFormatDistance(new Date(notification.created_at), new Date(), {
+                            locale: locale,
+                          })}
+                        </p>
+                        {!notification.read_at && (
+                          <Button
+                            onClick={() => {
+                              setSelectedId(notification.id);
+                              mutate(notification.id);
+                            }}
+                            disabled={isPending && selectedId === notification.id}
+                            className="absolute end-1 top-1 size-6 rounded-sm"
+                          >
+                            {isPending && selectedId === notification.id ? (
+                              <Loader2 className="size-4 animate-spin" />
+                            ) : (
+                              <CheckCheck className="size-4" />
+                            )}
+                          </Button>
+                        )}
+                      </div>
+                    )}
+                  </React.Fragment>
+                ))}
+              </div>
+            </InfiniteScroll>
+          ) : (
+            <p className="font-lg text-center font-medium text-red-500">No Notifications Found</p>
+          )}
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
--- src/lib/actions/notification.actions.ts
+++ src/lib/actions/notification.actions.ts
@@ -1,9 +1,48 @@
 "use server";
 
+type RepairRequestData = {
+  message: string;
+  code: string;
+  date: string; // "2025-09-14 15:30:41"
+};
+
+type CustomNotificationData = {
+  title: string;
+  body: string;
+  product_id: number;
+  image: string;
+};
+
+type BaseNotification<T extends string, D> = {
+  id: string;
+  type: T;
+  data: D;
+  read_at: string | null;
+  created_at: string;
+};
+
+export type Notification =
+  | BaseNotification<"RepairRequestNotification", RepairRequestData>
+  | BaseNotification<"CustomNotification", CustomNotificationData>;
+
+export type NotificationResponse = {
+  success: boolean;
+  message: string;
+  data: {
+    data: Notification[];
+    pagination: {
+      page: number;
+      total_pages: number;
+      total_items: number;
+      limit: number;
+    };
+  };
+};
+
 import { getLocale } from "next-intl/server";
 import { cookies } from "next/headers";
 
-export const getUserNotifications = async () => {
+export const getUserNotifications = async ({ pageParam = 0 }) => {
   const cookieStore = await cookies();
   const token = cookieStore.get("auth_token")?.value;
   const locale = await getLocale();
@@ -13,19 +52,22 @@ export const getUserNotifications = async () => {
   }
 
   try {
-    const response = await fetch(`${process.env.API}user/noifications`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        lang: locale || "ar",
+    const response = await fetch(
+      `${process.env.API}user/noifications?per_page=7&page=${pageParam}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          lang: locale || "ar",
+        },
       },
-    });
+    );
 
     const payload = await response.json();
     if (!response.ok) {
       throw new Error(payload.message || "Error fetching notifications");
     }
 
-    return payload;
+    return payload as NotificationResponse;
   } catch (err) {
     throw err;
   }
