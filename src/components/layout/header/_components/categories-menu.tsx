625adb92872ca8cab5d1a7e65323cd716c6484f8
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-09-17 15:05:33 +0300

    categories dropdown infinite scroll

--- src/components/layout/header/_components/categories-menu.tsx
+++ src/components/layout/header/_components/categories-menu.tsx
@@ -7,12 +7,29 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getPaginatedCategories } from "@/lib/actions/category.actions";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export default function CategoriesMenu({ categories }: { categories: Category[] }) {
+export default function CategoriesMenu() {
   // Translation
   const t = useTranslations();
+
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: getPaginatedCategories,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      const isLastPage = lastPage.data.meta.current_page >= lastPage.data.meta.last_page;
+      return isLastPage ? undefined : lastPage.data.meta.current_page + 1;
+    },
+  });
+
+  const allCategoires = data?.pages.flatMap((page) => page.data.data) ?? [];
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -20,24 +37,50 @@ export default function CategoriesMenu({ categories }: { categories: Category[] 
           <NavigationMenuTrigger className="cursor-pointer text-base font-medium rtl:font-bold xl:rtl:text-xl">
             {t("categories")}
           </NavigationMenuTrigger>
-          <NavigationMenuContent className="max-h-[250px] overflow-y-auto text-nowrap md:left-1/2 md:-translate-x-1/2 rtl:flex-row-reverse">
-            <div
-              className="grid w-full grid-cols-2 gap-3"
-              style={{
-                gridTemplateRows: "repeat(4, auto)",
-                gridTemplateColumns: "repeat(2, auto)",
-              }}
-            >
-              {categories.map((category, index) => (
-                <Link
-                  key={index}
-                  href={`/categories/${category.name}/${category.id}`}
-                  className="hover:bg-main w-full rounded-md bg-zinc-100 p-2 text-sm font-medium text-gray-900 transition-all duration-300 hover:text-white"
-                >
-                  {category.name}
-                </Link>
+          <NavigationMenuContent
+            id="scrollTarget"
+            className="max-h-[200px] !w-[400px] overflow-y-auto md:left-1/2 md:-translate-x-1/2 rtl:flex-row-reverse"
+          >
+            {isLoading && (
+              <div
+                className="grid w-full grid-cols-2 gap-3"
+                style={{
+                  gridTemplateColumns: "repeat(2, auto)",
+                  gridTemplateRows: "repeat(2, auto)",
+                }}
+              >
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <Skeleton key={i} className="h-10 w-full rounded-md"></Skeleton>
+                ))}
+              </div>
+            )}
+
+            <InfiniteScroll
+              dataLength={allCategoires.length}
+              next={fetchNextPage}
+              hasMore={!!hasNextPage}
+              loader={Array.from({ length: 2 }).map((_, i) => (
+                <Skeleton key={i} className="mt-3 h-10 w-full rounded-md"></Skeleton>
               ))}
-            </div>
+              scrollableTarget="scrollTarget"
+            >
+              <div
+                className="grid w-full grid-cols-2 gap-3"
+                style={{
+                  gridTemplateColumns: "repeat(2, auto)",
+                }}
+              >
+                {allCategoires.map((category, index) => (
+                  <Link
+                    key={index}
+                    href={`/categories/${category.name}/${category.id}`}
+                    className="hover:bg-main rounded-md bg-zinc-100 p-2 text-sm font-medium text-gray-900 transition-all duration-300 hover:text-white"
+                  >
+                    {category.name}
+                  </Link>
+                ))}
+              </div>
+            </InfiniteScroll>
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
--- src/lib/actions/category.actions.ts
+++ src/lib/actions/category.actions.ts
@@ -20,3 +20,51 @@ export const getAllCategories = async (limit = 10) => {
   const data = await response.json();
   return data.data.data;
 };
+
+export type Category = {
+  id: number;
+  name: string;
+  tags: string[];
+  image: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type PaginationMeta = {
+  current_page: number;
+  from: number;
+  last_page: number;
+  path: string;
+  per_page: number;
+  to: number;
+  total: number;
+};
+
+export type CategoryResponse = {
+  success: boolean;
+  message: string;
+  data: {
+    data: Category[];
+    meta: PaginationMeta;
+  };
+};
+
+export const getPaginatedCategories = async ({ pageParam = 0 }) => {
+  const t = await getTranslations();
+  const lang = await getLocale();
+
+  const response = await fetch(`${process.env.API}categories?limit=8&page=${pageParam}`, {
+    headers: {
+      "Content-Type": "application/json",
+      lang,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(t("failed-to-fetch-data"));
+  }
+
+  const payload: CategoryResponse = await response.json();
+  return payload;
+};
